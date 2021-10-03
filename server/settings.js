df02ac6089efc2791f383bfb84e1d956cef74050
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2021-10-03 17:38:53 +0200

    edit db ssl options by environment variable PROCESS_ENVIRONMENT

--- server/settings.js
+++ server/settings.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     staticFiles: 'static',
     DB: process.env.DATABASE_URL,
-    BOT_TOKEN: process.env.BOT_TOKEN
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    SSL: (process.env.PROCESS_ENVIRONMENT == 'PRODUCTION' ? { rejectUnauthorized: false } : false)
 }
